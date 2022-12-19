cdd8d9ebce77e89cb808198faab002290e737436
chaudharirohit25 <rohitchaudhari25@gmail.com>
2022-12-19 07:39:53 +0000

    app.py file updated for paths of file.

--- app.py
+++ app.py
@@ -6,20 +6,25 @@ from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.models import load_model
 import os
 
+print("inside app.py")
 app = Flask(__name__)
-
+print("flask app created.")
 @app.route('/', methods = ['POST', 'GET'])
 def form():
-    full_filename = '/opt/static/IMG/CDAC_logo.jpeg'
+    print("inside form")
+    full_filename = '/static/IMG/CDAC_logo.jpeg'
     return render_template('form.html')
-
+print("form function created.")
 @app.route('/data', methods = ['POST', 'GET'])
 def data():
-    full_filename = '/opt/static/IMG/CDAC_logo.jpeg'
+    print("inside data.")
+    full_filename = '/static/IMG/CDAC_logo.jpeg'
     form_data = request.form
     preds = query(form_data)
     return render_template('data.html',form_data = form_data,
                              pred_value=preds)
+print("data function created.")
 
 if __name__=='__main__':
     app.run(host='0.0.0.0',port=5000)
+print("In main function")
